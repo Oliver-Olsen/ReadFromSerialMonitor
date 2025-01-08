8ef7597499219d508ab2fbf4737f293a45d3e425
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-08 10:21:06 +0100

    Solution 6a

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/*  6a. char is a character. This is not only used to represent a letter
+*   but also a number like eg. 4 as a character. So it is another way to
+*   represent/handle letter number or symbols.
+*/
+
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
