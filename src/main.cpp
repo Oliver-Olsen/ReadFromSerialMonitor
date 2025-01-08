045285313f901cb720bf8458a0615d2b06a24055
Metafysical <wulff43211234@gmail.com>
2025-01-08 10:34:43 +0100

    Added code for LED decoding

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,78 @@
 *   represent/handle letter number or symbols.
 */
 
+#define PIN_A 6
+#define PIN_B 5
+#define PIN_C 4
+#define PIN_D 3
+#define PIN_E 2
 
+int pins[5] = {
+  PIN_A, 
+  PIN_B, 
+  PIN_C, 
+  PIN_D, 
+  PIN_E
+}; 
 
 
 void setup() {
+for (int i = 0; i < 5; i++) {
+  pinMode(pins[i], OUTPUT); 
+  digitalWrite(pins[i], LOW); 
+}
+
 
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    char msg = Serial.read(); 
+
+    switch (msg)
+    {
+    case 'a':
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(pins[i], LOW); 
+      }
+      digitalWrite(PIN_A, HIGH); 
+      break;
+
+    case 'b':
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(pins[i], LOW); 
+      }
+      digitalWrite(PIN_B, HIGH); 
+      break;
+    
+    case 'c':
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(pins[i], LOW); 
+      }
+      digitalWrite(PIN_C, HIGH); 
+      break;
+
+    case 'd':
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(pins[i], LOW); 
+      }
+      digitalWrite(PIN_D, HIGH); 
+      break;
+
+    case 'e':
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(pins[i], LOW); 
+      }
+      digitalWrite(PIN_E, HIGH); 
+      break;
+
+    default:
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(pins[i], LOW); 
+      }
+      break;
+    }
+  }
 }
 
